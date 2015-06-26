9501396b4e2852d0fc17c9722635bc6ba34d783c
Ilya Radchenko <ilya@burstcreations.com>
2015-06-26 17:51:58 -0400

    Add plugin prompts for init

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
+  prompts: {
+    init: require('./lib/prompts/init')
+  },
   init: require('./lib/init'),
   deploy: require('./lib/deploy'),
   remove: require('./lib/remove')
